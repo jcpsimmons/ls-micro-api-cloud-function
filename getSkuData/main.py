56737321af09b8afe7e82a3ec9e9badaec60b048
Josh <jcpsimmons@gmail.com>
2019-08-27 09:21:33 -0700

    I wasn't calling the right fcn haha

--- getSkuData/main.py
+++ getSkuData/main.py
@@ -2,16 +2,20 @@ from google.cloud import firestore
 from datetime import datetime
 import json
 
+
 def getSkuData(request):
     db = firestore.Client()
-    responseDict = {}
+    responseDict = {'data': {}}
     request_json = request.get_json()
     if request_json['sku'].find(',') > 0:
         skus = request_json['sku'].split(',')
     else:
         skus = [request_json['sku']]
     for sku in skus:
-        doc = db.collection('ls-restful-api-scrape').document(sku).get()
-        responseDict[doc.id] = doc.to_dict()
+        try:
+            doc = db.collection('ls-restful-api-scrape').document(sku).get()
+            responseDict['data'][str(doc.id)] = doc.to_dict()
+        except:
+            responseDict['data'][str(sku)] = 'Not found'
     response_dict = json.dumps(responseDict)
     return(response_dict)
---
+++ getSkuData/rm.py
@@ -0,0 +1,18 @@
+from google.cloud import firestore
+from datetime import datetime
+import json
+
+
+def getSkuRequest(request):
+    db = firestore.Client()
+    responseDict = {}
+    request_json = request.get_json()
+    if request_json['sku'].find(',') > 0:
+        skus = request_json['sku'].split(',')
+    else:
+        skus = [request_json['sku']]
+    for sku in skus:
+        doc = db.collection('ls-restful-api-scrape').document(sku).get()
+        responseDict[doc.id] = doc.to_dict()
+    response_dict = json.dumps(responseDict)
+    return(response_dict)
